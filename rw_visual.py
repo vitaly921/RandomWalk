f05d489169b4e655e834aad223c6eb0ed452a3bf
Виталий Приходьков <prihodkov21vek@gmail.com>
2025-04-13 15:20:47 +0300

    fix: - Fixed inactive Create button when launching the program; style: - Comments added to the program code.

--- rw_visual.py
+++ rw_visual.py
@@ -22,21 +22,20 @@ def build_graphs():
 
 def build_static_graph():
     """Функция построения линейных и точечных графиков в виде изображения"""
+    # Объявление глобальных переменных
+    global points_size_var
+    global line_size_var
+    global points_colormap_var
+    global points_color_var
+    global colormap_var
+    global line_color_var
+
     # Закрытие всех активных окон
     plt.close('all')
     # Сохранение в виде целого числа кол-ва точек от пользователя
     count_points = int(count_points_entry.get())
     # Сохранение в виде целого числа максимальной длины шага от пользователя
     max_step = int(max_step_entry.get())
-    # Объявление глобальной переменной размера точек
-    global points_size_var
-    global line_size_var
-    #
-    global points_colormap_var
-    global points_color_var
-    global colormap_var
-    global line_color_var
-
 
     # Создание экземпляра с регулируемой длиной списка координат точек
     rw = RandomWalk(count_points, max_step)
@@ -48,8 +47,6 @@ def build_static_graph():
     # Определение расстояния между первой и последней точками
     distance = math.sqrt((rw.x_values[-1] - rw.x_values[0]) ** 2 + (rw.y_values[-1] - rw.y_values[0]) ** 2)
 
-
-
     # Если выбран точечный график
     if points_var.get():
         # Задание размера окна для точечного графика
@@ -57,16 +54,19 @@ def build_static_graph():
 
         # Получение пользовательского значения размера точек
         size_points = int(points_size_var.get())
-        #
+        # Получение пользовательского значения цветовой карты точек
         points_colormap = points_colormap_var.get()
+        # Получение пользовательского значения цвета точек
         points_color = points_color_var.get()
+        # Получение пользовательского значения чек-бокса цветовой карты
         colormap_mode = colormap_var.get()
 
-        # --- Разделение на цветовую карту и обычный цвет и их выбор пользователем
+        # Разделение на цветовую карту и обычный цвет и их выбор пользователем
         if colormap_mode:
-            # Создание точечной диаграммы с вычисленными точками для X,Y
+            # Создание точечной диаграммы с вычисленными точками для X,Y с цветовой картой
             plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=points_colormap, edgecolors='none', s=size_points)
         else:
+            # Создание точечной диаграммы с вычисленными точками для X,Y определенного цвета
             plt.scatter(rw.x_values, rw.y_values, c=points_color, edgecolors='none',
                         s=size_points)
 
@@ -86,13 +86,12 @@ def build_static_graph():
     if line_var.get():
         # Задание размера окна для линейного графика
         plt.figure(figsize=(9, 5))
-        # ---Получение пользовательского значения ширины линии
-        # ---Получение пользовательского значения цвета линии
+        # Получение пользовательского значения ширины линии
         line_color = line_color_var.get()
+        # Получение пользовательского значения цвета линии
         line_size = line_size_var.get()
         # Создание линейной диаграммы с вычисленными точками для X, Y
         plt.plot(rw.x_values, rw.y_values, c=line_color, linewidth = line_size, zorder =1)
-
 
         # Отрисовка увеличенных начальной и конечной точек блуждания
         plt.scatter(0, 0, c='green', edgecolors='none', s=100, zorder =2)
@@ -114,41 +113,43 @@ def build_static_graph():
 
 def build_animation():
     """Функция построения графиков в виде анимации"""
+    # Объявление глобальных переменных
+    global points_size_var
+    global line_size_var
+    global colormap_var
+    global points_colormap_var
+    global points_color_var
+    global line_color_var
+
     # Закрытие всех активных окон
     plt.close('all')
     # Сохранение в виде целого числа кол-ва точек от пользователя
     count_points = int(count_points_entry.get())
     # Сохранение в виде целого числа максимальной длины шага от пользователя
     max_step = int(max_step_entry.get())
+    # Переменная для хранения состояния чек-бокса повторения анимации
+    repeat_animation = repeat_var.get()
     # Создание экземпляра с регулируемой длиной списка координат точек
     rw = RandomWalk(count_points, max_step)
     # Заполнение списка координатами
     rw.fill_walk()
-    #
-    global points_size_var
-    global line_size_var
-    global colormap_var
-    #
-    global points_colormap_var
-    global points_color_var
-    global line_color_var
-
-    # Переменная для хранения состояния чек-бокса повторения анимации
-    repeat_animation = repeat_var.get()
 
     # Если выбрана анимация линий
     if line_var.get():
+        # Получение пользовательских значений цвета линии
+        line_color = line_color_var.get()
+        # Получение пользовательских значений ширины линии
+        line_size = line_size_var.get()
+
         # Создание окна графика и осей
         fig, ax = plt.subplots(figsize=(8, 6))
-        line_color = line_color_var.get()
-        line_size = line_size_var.get()
+
         # Установка границ осей
         ax.set_xlim(min(rw.x_values) - 1, max(rw.x_values) + 1)
         ax.set_ylim(min(rw.y_values) - 1, max(rw.y_values) + 1)
 
         # Создание начальной точки и линии
         current_point_for_lines, = ax.plot(rw.x_values[0], rw.y_values[0], 'ro')
-
         line, = ax.plot([], [], c=line_color, linewidth=line_size)
 
         def init_line():
@@ -176,33 +177,38 @@ def build_animation():
 
     # Если выбрана анимация точек
     if points_var.get():
-        #
+        # Получение пользовательского значения размера точек
         size_points = int(points_size_var.get())
-        #
+        # Получение пользовательского значения чек-бокса цветовой карты
         colormap_mode = colormap_var.get()
+        # Получение пользовательского значения цветовой карты точек
         points_colormap = points_colormap_var.get()
+        # Получение пользовательского значения цвета точек
+        points_color = points_color_var.get()
+
         # Создание списка длиной количества точек
         point_numbers = list(range(rw.num_points))
-        points_color = points_color_var.get()
         # Создание окна графика и осей
         fig, ax = plt.subplots(figsize=(8, 6))
         # Установка границ осей
         ax.set_xlim(min(rw.x_values) - 1, max(rw.x_values) + 1)
         ax.set_ylim(min(rw.y_values) - 1, max(rw.y_values) + 1)
-        # Создание начальных точе
+        # Создание начальных точек
         #points, = ax.plot([], [], 'ro', markersize=(size_points**0.5))
         current_point, = ax.plot(rw.x_values[0], rw.y_values[0], 'ro')
 
+        # Обработка случая отображения с цветовой картой
         if colormap_mode:
             points = ax.scatter([], [], c=[], s=size_points, cmap=points_colormap, norm=plt.Normalize(vmin=0, vmax=len(point_numbers)))
+        # Обработка случая отображения без цветовой карты
         else:
             points = ax.scatter([], [], s=size_points, color=points_color)
-
 
         def init_points():
             """Функция инициализации"""
             # Установка пустых кадров для точек
             points.set_offsets([])
+            # Для случая отображения с цветовой картой
             if colormap_mode:
                 points.set_array([])
             current_point.set_data([], [])
@@ -213,7 +219,7 @@ def build_animation():
             # Обновление вида точек от начала и до текущего кадра
             #points.set_offsets([[x, y] for x, y in zip(rw.x_values[:frame+1], rw.y_values[:frame+1])])
             points.set_offsets(np.column_stack((rw.x_values[:frame+1], rw.y_values[:frame+1])))
-
+            # Для случая отображения с цветовой картой
             if colormap_mode:
                 points.set_array(np.array(point_numbers[:frame+1]))
             # Обновление вида точки на текущем кадре
@@ -230,23 +236,23 @@ def build_animation():
 
 
 def validate_count_points(input_value):
-    """Проверка ввода значений в поле count points окна настроек"""
+    """Валидация значений в поле count points окна настроек"""
     if input_value == "":  # Разрешаем пустое поле
         return True
-    if input_value.isdigit():
+    if input_value.isdigit(): # Для случая заполненного поля
         num = int(input_value)
-        if 0 <= num <= 5000:
+        if 0 <= num <= 5000:  # Проверка диапазона
             return True
     return False
 
 
 def validate_max_step(input_value):
-    """Проверка ввода значений в поле max_step окна настроек"""
+    """Валидация значений в поле max_step окна настроек"""
     if input_value == "":  # Разрешаем пустое поле
         return True
-    if input_value.isdigit():
+    if input_value.isdigit(): # Для случая заполненного поля
         num = int(input_value)
-        if 0 <= num <= 30:
+        if 0 <= num <= 30:    # Проверка диапазона
             return True
     return False
 
@@ -303,17 +309,25 @@ def update_settings_frame():
     key += ('_points' if is_points else '') + ('_lines' if is_lines else '')
 
     if key in frames:
-        labels[key].pack(anchor="nw", pady=(10, 0))  # Показываем заголовок
+        labels[key].pack(anchor="nw", pady=(10, 0))      # Показываем заголовок
         frames[key].pack(anchor="nw", fill="x", padx=5)  # Показываем фрейм
 
+    # Для случая выбора статичного отображения графика(-ов)
     if mode_var.get()=="static":
+        # Кнопка повтора анимации неактивная
         repeat_check.config(state="disabled")
     else:
+        # Кнопка повтора активна
         repeat_check.config(state="normal")
 
 
 def create_settings_frames(title, is_animation=False, has_points=True, has_lines=True):
     """Функция создания динамических фреймов для дополнительной настройки графиков"""
+    # Импорт цветов
+    colors = list(mcolors.CSS4_COLORS)
+    # Импорт списка вариантов цветовых карт
+    colormaps = plt.colormaps()
+
     # Создание заголовка фрейма
     label = ttk.Label(root, text=title, font=("Arial", 8))
     label.pack(pady=(10, 0))
@@ -322,45 +336,33 @@ def create_settings_frames(title, is_animation=False, has_points=True, has_lines
     frame = ttk.Frame(root, borderwidth=1, relief=SOLID, padding=[8, 10])
 
     def toggle_colormap():
-        """"""
+        """Обработка события выбора чек-бокса с цветовыми картами"""
+        # Если чек-бокс выбран, то кнопка выбора цветовой карт активна, а выбора простого цвета НЕ активна
         if colormap_var.get():
             colormap_combobox.config(state="readonly")
             color_combobox.config(state="disabled")
+        # Если чек-бокс НЕ выбран, то кнопка выбора цветовой карт НЕ активна, а выбора простого цвета активна
         else:
             colormap_combobox.config(state="disabled")
             color_combobox.config(state="normal")
 
+    #print(f"Creating frame: {title}, is_animation={is_animation}")
 
-
-    print(f"Creating frame: {title}, is_animation={is_animation}")
-
-    # Если при создании фрейма параметр is_animation=True
+   #  Динамический фрейм для создания настроек анимации графиков
    # if is_animation:
    #     # Импорт переменной со значением для чек-бокса повтора анимации (по-умолчанию False)
    #     global repeat_var
    #     # Отрисовка чек-бокса повтора анимации в выбранной части фрейма
    #     repeat_check = ttk.Checkbutton(frame, text="Repeating", variable=repeat_var)
    #     repeat_check.grid(row=0, column=0, sticky='w', padx=5, pady=2)
-#
-   # # Если при создании фрейма параметр is_animation=False
+   #  Динамический фрейм для создания настроек статического изображения графиков
    # if not is_animation:
    #     pass
 
-    # Импорт цветов
-    colors = list(mcolors.CSS4_COLORS)
-    # Если при создании фрейма параметр has_points=True
+
+    # Динамический фрейм для создания настроек точечного графика
     if has_points:
-        # Импорт переменной со значением размера точек от пользователя
-        global points_size_var
-        #
-        global points_colormap_var
-        global points_color_var
-        global colormap_var
-        # Импорт списка вариантов цветовых карт
-        colormaps = plt.colormaps()
-
-
-
+        # Создание чек-бокса выбора цветовой карты
         colormap_check = ttk.Checkbutton(frame, text="Colormap", variable=colormap_var, command=toggle_colormap)
         colormap_check.grid(row=1, column=0, sticky="w", padx=5, pady=2)
         # Создание надписи для выбора цветовой карты
@@ -385,7 +387,7 @@ def create_settings_frames(title, is_animation=False, has_points=True, has_lines
         #print(points_size.get())
         #point_count.set(1)
 
-    # Если при создании фрейма параметр has_lines=True
+    # Динамический фрейм для создания настроек линейного графика
     if has_lines:
         # Создание надписи для цвета линии
         ttk.Label(frame, text="Цвет линий: ").grid(row=3, column=0, sticky="w", padx=5, pady=2)
@@ -418,16 +420,20 @@ options = [
     ("animation_points_lines", "Настройки анимационных точек и линий", True, True, True),
 ]
 
+# ЗАДАНИЕ ЗНАЧЕНИЙ ПАРАМЕТРОВ ПО УМОЛЧАНИЮ
 # Создание булевой переменной повтора анимации со значением False
 repeat_var = tk.BooleanVar(value=False)
-# Создание целочисленной переменной размера точек со значением по-умолчанию
+# Создание целочисленных переменных размера точек и ширины линии
 points_size_var = tk.IntVar(value=5)
 line_size_var = tk.IntVar(value=5)
-#
+# Создание строковой переменной со значением для цветовой карты точек
 points_colormap_var = tk.StringVar(value="Greys")
+# Создание строковых переменных со значением цвета точек и линии
 points_color_var = tk.StringVar(value="blue")
 line_color_var = tk.StringVar(value="blue")
+# Создание целочисленной переменной со значением максимальной длины шага
 max_step_var = tk.IntVar(value=5)
+# Создание целочисленной переменной со значением кол-ва точек
 count_points_var = tk.IntVar(value=500)
 # Создание чек-бокса цветовой карты
 colormap_var = tk.BooleanVar(value=False)
@@ -440,7 +446,6 @@ for key, title, is_animation, has_points, has_lines in options:
 
 # Создание надписи на главном окне
 ttk.Label(root, text='Settings Random Walk', font=("Arial", 14)).pack(pady=2)
-
 
 # Блок создания фрейма для наполнения числовыми данными
 #-----------------------------------------------------------------------------------------------------------------------
@@ -473,8 +478,8 @@ max_step_entry.bind_all("<Key>", update_button_state)
 # Блок создания фрейма для выбора вида графика
 #-----------------------------------------------------------------------------------------------------------------------
 # Переменные-флаги в значении True (выбраны по умолчанию два вида представления)
-line_var = tk.BooleanVar(value=True)
-points_var = tk.BooleanVar(value=True)
+line_var = tk.BooleanVar(value=False)
+points_var = tk.BooleanVar(value=False)
 
 # Отслеживание изменения в переменных и при их возникновении вызов лямбда-функции для создания дополнительных
 # динамических фреймов с настройками в зависимости от вида графика
